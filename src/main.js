fe3150a6e969c6728d1c3dec18229cc6345bdeab
Sn0wF1re <mokayaclinton34@gmail.com>
2025-03-13 11:50:06 +0300

    Add google analytics

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/styles.css'
+import VueGtag from "vue-gtag";
+
+app.use(VueGtag, {
+    config: { id: 'G-WXVJNCHQ6S' }  // Your Google Analytics ID
+}, router);
+
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    headers: {
+      'Cache-Control': 'max-age=31536000'
+    }
+  }
 })
